5590f37b62db8f6fea2dea9a507023eafb38964c
Ishaan2008-op <ishaanram97@gmail.com>
2026-04-12 11:31:06 +0530

    new commit fresh start

---
+++ applogic.py
@@ -0,0 +1,6 @@
+#" here we will write the code using pandas , matplotlib  and seaborn to show  the outcomes in real time"
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import flask
+#solve the issue of muntiple team names which are same but different in spelling
